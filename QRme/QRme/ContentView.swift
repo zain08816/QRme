de678759d94ae2e379b2ea0b261a74c8e14970ae
Zain Ali <zain08816@gmail.com>
2020-01-02 15:29:35 -0700

    create initial project

---
+++ QRme/QRme/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  QRme
+//
+//  Created by Zain Ali on 1/2/20.
+//  Copyright © 2020 Zain Ali. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
