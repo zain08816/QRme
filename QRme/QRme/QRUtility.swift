60b7d42d6e7139c169a0fe67369fff2885ebb792
Zain Ali <zain08816@gmail.com>
2020-01-05 18:23:41 -0700

    add QRGenerator

---
+++ QRme/QRme/QRUtility.swift
@@ -0,0 +1,30 @@
+//
+//  QRUtility.swift
+//  QRme
+//
+//  Created by Zain Ali on 1/5/20.
+//  Copyright © 2020 Zain Ali. All rights reserved.
+//
+
+import Foundation
+
+class QR {
+        
+    var qrCodeString =
+    "BEGIN:VCARD \n" +
+    "VERSION:4.0 \n" +
+    "N:Peter;John \n" +
+    "FN:John Peter \n" +
+    "TITLE:Admin \n" +
+    "TEL;TYPE=cell:+91 431 524 2345 \n" +
+    "EMAIL::John@ommail.in \n" +
+    "URL:www.facebook.com \n" +
+    "URL: www.instagram.com \n" +
+    "ADR;WORK:;;423 ofce sales Center;Newark;DE;3243;USA \n" +
+    "END:VCARD"
+
+
+    
+    
+    
+}
