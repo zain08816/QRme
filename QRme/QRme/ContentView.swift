46cb1b3a1ba91fbadc6df116a06095385e999690
Zain Ali <zain08816@gmail.com>
2020-01-05 18:24:01 -0700

    add nav bar

--- QRme/QRme/ContentView.swift
+++ QRme/QRme/ContentView.swift
@@ -9,10 +9,67 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
+    @State var name: String = ""
+    @State private var selection = 0
+    
     var body: some View {
-        Text("Hello, World!")
+        TabView(selection: $selection){
+            DataInput()
+                .tabItem {
+                    VStack {
+                         Image(systemName: "person.crop.square")
+                        Text("Information")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image(systemName: "qrcode")
+                        Text("QRCode")
+                    }
+                }
+                .tag(1)
+        }.accentColor(.accentColor)
         
+        
+//        VStack {
+//
+//            TextField(" Enter your name", text: $name).border(Color.blue)
+//            Text("Hello, \(name)!")
+//        }
     }
+
+    func generateQRCode(from string: String) -> UIImage? {
+
+        let data = string.data(using: String.Encoding.ascii)
+        var uiImage: UIImage?
+        if let filter = CIFilter(name: "CIQRCodeGenerator",
+                                 parameters: ["inputMessage": data as Any,
+                                              "inputCorrectionLevel": "L"]) {
+
+            if let outputImage = filter.outputImage,
+                let cgImage = CIContext().createCGImage(outputImage,
+                                                        from: outputImage.extent) {
+                let size = CGSize(width: outputImage.extent.width * 3.0,
+                                  height: outputImage.extent.height * 3.0)
+                UIGraphicsBeginImageContext(size)
+                if let context = UIGraphicsGetCurrentContext() {
+                    context.interpolationQuality = .none
+                    context.draw(cgImage,
+                                 in: CGRect(origin: .zero,
+                                            size: size))
+                    uiImage = UIGraphicsGetImageFromCurrentImageContext()
+                }
+                UIGraphicsEndImageContext()
+            }
+        }
+        return uiImage
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
